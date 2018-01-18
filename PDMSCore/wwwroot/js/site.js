8728206e0ea05e987f9c9c6007f272ce563a794c
Martin Svarovsky <msvarovsky@gmail.com>
2018-01-18 15:58:59 +0000

    a

--- PDMSCore/wwwroot/js/site.js
+++ PDMSCore/wwwroot/js/site.js
@@ -1,1 +1,71 @@
-﻿// Write your JavaScript code.
+﻿/*
+    http://www.designchemical.com/blog/index.php/jquery/jquery-simple-vertical-accordion-menu/
+    */
+/*  http://jsfiddle.net/yeyene/Bx5cu/29/  */
+$(document).ready(function () {
+
+
+    $('li ul').slideUp();
+    $('.no-js li a.MainMenuLevel1').on("click", function () {
+        if ($(this).siblings('ul').is(":visible")) {
+            $(this).siblings('ul').slideUp(100);
+            $(this).removeClass("SelectedMenuItem");
+        }
+        else {
+            $('ul ul').slideUp(100);
+            $('.MainMenuLevel1').removeClass("SelectedMenuItem");
+            $(this).addClass("SelectedMenuItem");
+            $(this).siblings('ul').slideDown(200);
+        }
+        return false;
+    });
+
+    $('.no-js li a.MainMenuLevel2').on("click", function () {
+        if ($(this).siblings('ul').is(":visible")) {
+            $('ul ul ul').slideUp(400);
+            $(this).removeClass("SelectedMenuItem");
+        }
+        else {
+            $('ul ul ul').slideUp(100);
+            $('.MainMenuLevel2').removeClass("SelectedMenuItem");
+            $(this).addClass("SelectedMenuItem");
+            $(this).siblings('ul').slideDown(200);
+        }
+        return false;
+    });
+});
+
+//        https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_js_dropdown
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+$(function () {
+    $.datepicker.setDefaults({
+        //              showOn: "both",
+        firstDay: 1,
+        buttonImageOnly: true,
+        numberOfMonths: 2,
+        showWeek: true,
+        showAnim: "slideDown",
+        showButtonPanel: true
+    });
+
+
+    $("#datepicker").datepicker($.datepicker.regional["fr"]);
+});
