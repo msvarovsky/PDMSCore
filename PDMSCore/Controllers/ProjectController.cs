32b9af695ef06b7f4392022b53f3e1c6842f322c
Martin Svarovsky <msvarovsky@gmail.com>
2018-01-30 17:36:34 +0000

    json todo

--- PDMSCore/Controllers/ProjectController.cs
+++ PDMSCore/Controllers/ProjectController.cs
@@ -2,6 +2,13 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
 using System.Collections.Generic;
+using System;
+using System.Text;
+using PDMSCore.DataManipulation;
+using System.Runtime.Serialization.Json;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
 
 namespace PDMSCore.Controllers
 {
@@ -50,22 +57,69 @@ namespace PDMSCore.Controllers
             p = new Project();
 
             p.GetRandom();
-
-
-
             return View(p);
         }
 
+        [HttpPost]
+        public ActionResult ShowProject(IFormCollection fc)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var FieldId in fc.Keys)
+            {
+                sb.AppendLine(FieldId + ": " + fc[FieldId].ToString());
+            }
+
+            //p = Project.GetProject(PanelID);
+            p = new Project();
+
+            p.GetRandom();
+            return View(p);
+        }
+
+
+
+
         public JsonResult Ajax(string prefix, string id)
         {
-            List<string> allCompanyName = new List<string>();
-             
-            
-            allCompanyName.Add(id + ":ahoj-1");
-            allCompanyName.Add(id + "ahoj-2");
-            allCompanyName.Add(id + "ahoj-3");
-            
-            return Json(allCompanyName);
+            List<JsonItem> results = new List<JsonItem>();
+
+            int i = 0;
+
+            results.Add(new JsonItem(i++.ToString(), "Value-" + i));
+            results.Add(new JsonItem(i++.ToString(), "Value-" + i));
+            results.Add(new JsonItem(i++.ToString(), "Value-" + i));
+
+            dynamic product = new JObject();
+            product.ProductName = "Elbow Grease";
+            product.Enabled = true;
+            product.Price = 4.90m;
+            product.StockCount = 9000;
+            product.StockValue = 44100;
+            product.Tags = new JArray("Real", "OnSale");
+            Console.WriteLine(product.ToString());
+
+            string r = JsonConvert.SerializeObject(results, Formatting.Indented);
+
+            //string r = ToJson<List<JsonItem>>(results, Encoding.UTF8);
+            //return Json(results);
+            //return Json(r.Trim());
+            return Json("{ \"ID \" : \"Name\"");
+        }
+
+        public static string ToJson<T>(/* this */ T value, Encoding encoding)
+        {
+            var serializer = new DataContractJsonSerializer(typeof(T));
+
+            using (var stream = new MemoryStream())
+            {
+                using (var writer = JsonReaderWriterFactory.CreateJsonWriter(stream, encoding))
+                {
+                    serializer.WriteObject(writer, value);
+                }
+
+                return encoding.GetString(stream.ToArray());
+            }
         }
 
         [HttpPost]
---
+++ PDMSCore/DataManipulation/JsonItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PDMSCore.DataManipulation
+{
+    public class JsonItem
+    {
+        public string ID { get; set; }
+        public string Value { get; set; }
+
+        public JsonItem(string ID, string Value)
+        {
+            this.ID = ID;
+            this.Value = Value;
+        }
+    }
+}
